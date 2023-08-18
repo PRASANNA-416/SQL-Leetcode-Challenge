14458295de1614332149e75d7bfb3cd030738239
Prasanna <76594416+PRASANNA-416@users.noreply.github.com>
2023-08-18 08:41:41 +0530

    Update Triangle Judgement.sql

--- Easy/Triangle Judgement.sql
+++ Easy/Triangle Judgement.sql
@@ -21,10 +21,5 @@
 
 
 -- Solution
-select x, y, z,
-case 
-when x+y > z and x+z > y and  y+z > x  then 'Yes' 
-when x=y and y=z then 'Yes'
-else 'No'
-end as Triangle
-from triangle
+SELECT *, CASE WHEN x+y>z AND y+z>x AND x+z>y THEN 'Yes' ELSE 'No' END AS triangle
+FROM Triangle
